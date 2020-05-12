7a333c77dcae55c28c67b607fb8d99ac11887068
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-12 13:07:49 +0530

    Create Java_Substring.java
    Corresponding problem and relevant information can be found in HackerRank here - https://www.hackerrank.com/challenges/java-substring/problem

---
+++ practice_Java/Strings/Java_Substring.java
@@ -0,0 +1,16 @@
+package practice_Java.Strings;
+
+import java.util.Scanner;
+
+public class Java_Substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+	}
+
+}

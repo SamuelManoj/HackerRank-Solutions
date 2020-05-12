80195e450ee8cdedd82f9531a27a2641b67ecca7
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-12 12:58:07 +0530

    Create Java_Stdin_and_Stdout_I.java
    Corresponding problem and relevant information can be found in HackerRank here - https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem

---
+++ practice_Java/Introduction/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,23 @@
+package practice_Java.Introduction;
+
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		// Complete this line
+		// Complete this line
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+		System.out.println(a);
+		// Complete this line
+		// Complete this line
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}

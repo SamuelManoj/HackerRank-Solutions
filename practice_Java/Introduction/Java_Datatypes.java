45ef1341dc855b7596cab60373b6c53a82e8e65d
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-12 13:02:52 +0530

    Create Java_Datatypes.java
    Corresponding problem and relevant information can be found in HackerRank here - https://www.hackerrank.com/challenges/java-datatypes/problem

---
+++ practice_Java/Introduction/Java_Datatypes.java
@@ -0,0 +1,32 @@
+package practice_Java.Introduction;
+
+import java.util.Scanner;
+
+public class Java_Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			try {
+				long x = sc.nextLong();
+				System.out.println(x + " can be fitted in:");
+				if (x >= -128 && x <= 127)
+					System.out.println("* byte");
+				// Complete the code
+				if (x >= - Math.pow(2, 15) && x <= Math.pow(2, 15)-1)
+					System.out.println("* short");
+				if (x >= - Math.pow(2, 31) && x <= Math.pow(2, 31)-1)
+					System.out.println("* int");
+				if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63)-1)
+					System.out.println("* long");
+				
+			} catch (Exception e) {
+				System.out.println(sc.next() + " can't be fitted anywhere.");
+			}
+		}
+	}
+
+}

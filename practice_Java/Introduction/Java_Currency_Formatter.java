8582cb3eb372c7e84ebf6dbc9add75751ecba0d8
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-12 13:05:36 +0530

    Create Java_Currency_Formatter.java
    Corresponding problem and relevant information can be found in HackerRank here - https://www.hackerrank.com/challenges/java-currency-formatter/problem

---
+++ practice_Java/Introduction/Java_Currency_Formatter.java
@@ -0,0 +1,31 @@
+package practice_Java.Introduction;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java_Currency_Formatter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		double payment = scanner.nextDouble();
+		scanner.close();
+
+		// Write your code here.
+		Locale INDIA = new Locale("en", "IN");
+		NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+		NumberFormat india = NumberFormat.getCurrencyInstance(INDIA);
+		NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+		NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+		System.out.println("US: " + us.format(payment));
+		System.out.println("India: " + india.format(payment));
+		System.out.println("China: " + china.format(payment));
+		System.out.println("France: " + france.format(payment));
+
+	}
+
+}

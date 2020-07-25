33885e144911cd43b8b36f93453434c0e3cb5700
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-07-25 18:29:53 +0530

    Create Solution.java

---
+++ practice_Java/Exception Handling/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+		 * class should be named Solution.
+		 */
+		Scanner scan = new Scanner(System.in);
+		try {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			System.out.println(x / y);
+		} catch (InputMismatchException e) {
+			System.out.println("java.util.InputMismatchException");
+		} catch (ArithmeticException e) {
+			System.out.println("java.lang.ArithmeticException: / by zero");
+		}
+		scan.close();
+	}
+}
